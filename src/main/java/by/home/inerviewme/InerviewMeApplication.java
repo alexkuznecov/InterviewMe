9771ed6b683c22da041c796dc6cac395c2508f72
Alexander Kuznetsov <aliaksandar.kuznetsou@azati.com>
2023-08-20 19:30:02 +0300

    init

---
+++ src/main/java/by/home/inerviewme/InerviewMeApplication.java
@@ -0,0 +1,13 @@
+package by.home.inerviewme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InerviewMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InerviewMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/home/inerviewme/InerviewMeApplicationTests.java
@@ -0,0 +1,13 @@
+package by.home.inerviewme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InerviewMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
